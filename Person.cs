fa9b59f0fbc01bf4d7c1666b579382fe1d29c9ab
hyconcodes <laycon.devs@gmail.com>
2026-05-21 16:02:45 +0100

    Separate the Person Class into another class file...

---
+++ Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        private int age;
+        public int Month { get; set; } 
+        public string PhoneNumber { get; set; } 
+        public string Username { get; set; } 
+        public string Password { get; set; } 
+        public int MatricNumber { get; set; } 
+        public string Department { get; set; }
+        public int Age{
+            get { return age; }
+            //set { age = value > 0 ? value : 0; }
+            set {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    age = 0;
+                }
+            }
+        }
+        public bool IsAdult{
+            get { return Age >= 18; }
+        }
+
+        public static string DisplayInfo()
+        {
+            return "Hello Hycon";
+        }
+
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using OOP;
+using System;
 
 internal class OOPV2
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("WELCOME TO OOP LESSON 2");
+        Console.WriteLine($"{Person.DisplayInfo()}, WELCOME TO OOP LESSON 2");
+        Thread.Sleep( 1000 );
+
+        string[] cars = { "Benz", "Toyota", "Mustang" };
+
+        foreach (var item in cars)
+        {
+            Console.WriteLine(item);
+            Thread.Sleep(1000);
+        }
     }
 }
