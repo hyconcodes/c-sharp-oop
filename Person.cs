1512da32f50743fa7270ae02c298872a8fc3648b
hyconcodes <laycon.devs@gmail.com>
2026-05-22 14:09:57 +0100

    using List in classes...

--- Person.cs
+++ Person.cs
@@ -32,9 +32,66 @@ namespace OOP
             get { return Age >= 18; }
         }
 
-        public static string DisplayInfo()
+        public string DisplayInfo()
         {
-            return "Hello Hycon";
+            string monthInWord = ReturnMonthInWord(Month);
+            return $"Name: {Name} \n" +
+                $"Age: {Age} \n" +
+                $"Month: {monthInWord} \n" +
+                $"Matric Number: {MatricNumber} \n" +
+                $"You're an adult: {IsAdult} \n";
+        }
+
+        static string ReturnMonthInWord(int monthNo)
+        {
+            string month = "";
+            switch (monthNo)
+            {
+                case 1:
+                    month = "January";
+                    break;
+                case 2:
+                    month = "Febuary";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "September";
+                    break;
+                case 10:
+                    month = "October";
+                    break;
+                case 11:
+                    month = "November";
+                    break;
+                case 12:
+                    month = "December";
+                    break;
+                //case var m when m <= 0 :
+                //    month = "Can't be less than 0";
+                //    break;
+                default:
+                    month = "Invalid...";
+                    break;
+            }
+
+            return month;
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -5,15 +5,29 @@ internal class OOPV2
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"{Person.DisplayInfo()}, WELCOME TO OOP LESSON 2");
-        Thread.Sleep( 1000 );
-
-        string[] cars = { "Benz", "Toyota", "Mustang" };
-
-        foreach (var item in cars)
+        List<Person> student = new List<Person>()
         {
-            Console.WriteLine(item);
-            Thread.Sleep(1000);
+            new Person
+            {
+                Name = "Hycon",
+                Age = 25,
+                Month = 9,
+                MatricNumber = 2782,
+            },
+            new Person
+            {
+                Name = "Olamide",
+                Age = 17,
+                Month = 5,
+                MatricNumber = 2780,
+            }
+
+        };
+
+        foreach (var s in student)
+        {
+        Console.WriteLine($"{s.DisplayInfo()} \nWELCOME TO ADVANCED OOP LESSON 2\n");
         }
+        
     }
 }
